c3afc2c6f2d12797e7b35c35e8fe9a35fe88ba05
Alberto Magno <am.magno2@gmail.com>
2025-06-12 14:33:09 -0300

    Add KPack01_Analyzer

---
+++ KPack01_Analyzer/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace KPack01_Analyzer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            selectExeButton = new Button();
+            exePathTextBox = new TextBox();
+            initialItemsNumeric = new NumericUpDown();
+            executionCountNumeric = new NumericUpDown();
+            initialCapacityNumeric = new NumericUpDown();
+            resultsDataGridView = new DataGridView();
+            colInstance = new DataGridViewTextBoxColumn();
+            colItems = new DataGridViewTextBoxColumn();
+            colCapacity = new DataGridViewTextBoxColumn();
+            colTime = new DataGridViewTextBoxColumn();
+            colMemory = new DataGridViewTextBoxColumn();
+            colExpectedValue = new DataGridViewTextBoxColumn();
+            colSolutionValue = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            capacityIncrementNumeric = new NumericUpDown();
+            itemsIncrementNumeric = new NumericUpDown();
+            label4 = new Label();
+            label5 = new Label();
+            performanceChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label6 = new Label();
+            label7 = new Label();
+            executeButton = new Button();
+            richTextBox1 = new RichTextBox();
+            complexityLabel = new Label();
+            asymptoticBehaviorLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)initialItemsNumeric).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)executionCountNumeric).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)initialCapacityNumeric).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)resultsDataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)capacityIncrementNumeric).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)itemsIncrementNumeric).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)performanceChart).BeginInit();
+            SuspendLayout();
+            // 
+            // selectExeButton
+            // 
+            selectExeButton.Location = new Point(12, 12);
+            selectExeButton.Name = "selectExeButton";
+            selectExeButton.Size = new Size(136, 23);
+            selectExeButton.TabIndex = 0;
+            selectExeButton.Text = "Selecionar algoritmo";
+            selectExeButton.UseVisualStyleBackColor = true;
+            selectExeButton.Click += selectExeButton_Click;
+            // 
+            // exePathTextBox
+            // 
+            exePathTextBox.Location = new Point(154, 12);
+            exePathTextBox.Name = "exePathTextBox";
+            exePathTextBox.ReadOnly = true;
+            exePathTextBox.Size = new Size(453, 23);
+            exePathTextBox.TabIndex = 1;
+            // 
+            // initialItemsNumeric
+            // 
+            initialItemsNumeric.Location = new Point(140, 101);
+            initialItemsNumeric.Name = "initialItemsNumeric";
+            initialItemsNumeric.Size = new Size(51, 23);
+            initialItemsNumeric.TabIndex = 3;
+            // 
+            // executionCountNumeric
+            // 
+            executionCountNumeric.Location = new Point(613, 14);
+            executionCountNumeric.Name = "executionCountNumeric";
+            executionCountNumeric.Size = new Size(51, 23);
+            executionCountNumeric.TabIndex = 4;
+            // 
+            // initialCapacityNumeric
+            // 
+            initialCapacityNumeric.Location = new Point(140, 72);
+            initialCapacityNumeric.Name = "initialCapacityNumeric";
+            initialCapacityNumeric.Size = new Size(51, 23);
+            initialCapacityNumeric.TabIndex = 5;
+            // 
+            // resultsDataGridView
+            // 
+            resultsDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resultsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resultsDataGridView.Columns.AddRange(new DataGridViewColumn[] { colInstance, colItems, colCapacity, colTime, colMemory, colExpectedValue, colSolutionValue });
+            resultsDataGridView.Location = new Point(771, 181);
+            resultsDataGridView.Name = "resultsDataGridView";
+            resultsDataGridView.ReadOnly = true;
+            resultsDataGridView.Size = new Size(557, 379);
+            resultsDataGridView.TabIndex = 6;
+            // 
+            // colInstance
+            // 
+            colInstance.HeaderText = "Instância";
+            colInstance.Name = "colInstance";
+            colInstance.ReadOnly = true;
+            // 
+            // colItems
+            // 
+            colItems.HeaderText = "Itens";
+            colItems.Name = "colItems";
+            colItems.ReadOnly = true;
+            // 
+            // colCapacity
+            // 
+            colCapacity.HeaderText = "Capacidade";
+            colCapacity.Name = "colCapacity";
+            colCapacity.ReadOnly = true;
+            // 
+            // colTime
+            // 
+            colTime.HeaderText = "Tempo (ms)";
+            colTime.Name = "colTime";
+            colTime.ReadOnly = true;
+            // 
+            // colMemory
+            // 
+            colMemory.HeaderText = "Maior memoria";
+            colMemory.Name = "colMemory";
+            colMemory.ReadOnly = true;
+            // 
+            // colExpectedValue
+            // 
+            colExpectedValue.HeaderText = "Valor esperado";
+            colExpectedValue.Name = "colExpectedValue";
+            colExpectedValue.ReadOnly = true;
+            // 
+            // colSolutionValue
+            // 
+            colSolutionValue.HeaderText = "Valor Solução";
+            colSolutionValue.Name = "colSolutionValue";
+            colSolutionValue.ReadOnly = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Capacidade inicial:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(65, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Itens inicial:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(670, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(14, 15);
+            label3.TabIndex = 9;
+            label3.Text = "X";
+            // 
+            // capacityIncrementNumeric
+            // 
+            capacityIncrementNumeric.Location = new Point(327, 72);
+            capacityIncrementNumeric.Name = "capacityIncrementNumeric";
+            capacityIncrementNumeric.Size = new Size(51, 23);
+            capacityIncrementNumeric.TabIndex = 11;
+            // 
+            // itemsIncrementNumeric
+            // 
+            itemsIncrementNumeric.Location = new Point(327, 101);
+            itemsIncrementNumeric.Name = "itemsIncrementNumeric";
+            itemsIncrementNumeric.Size = new Size(51, 23);
+            itemsIncrementNumeric.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(200, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(121, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Capacidade intervalo:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(237, 103);
+            label5.Name = "label5";
+            label5.Size = new Size(84, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Itens intervalo:";
+            // 
+            // performanceChart
+            // 
+            chartArea1.AxisX.Title = "Número de Itens";
+            chartArea1.AxisY.Title = "Tempo (ms)";
+            chartArea1.Name = "ChartArea1";
+            performanceChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            performanceChart.Legends.Add(legend1);
+            performanceChart.Location = new Point(12, 130);
+            performanceChart.Name = "performanceChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "ExecutionTime";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "FittedCurve";
+            performanceChart.Series.Add(series1);
+            performanceChart.Series.Add(series2);
+            performanceChart.Size = new Size(753, 558);
+            performanceChart.TabIndex = 14;
+            performanceChart.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Grafico de performance";
+            performanceChart.Titles.Add(title1);
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(782, 148);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 15);
+            label6.TabIndex = 15;
+            label6.Text = "Complexidade:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(951, 148);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Assintotico:";
+            // 
+            // executeButton
+            // 
+            executeButton.Location = new Point(690, 16);
+            executeButton.Name = "executeButton";
+            executeButton.Size = new Size(75, 23);
+            executeButton.TabIndex = 19;
+            executeButton.Text = "Executar";
+            executeButton.UseVisualStyleBackColor = true;
+            executeButton.Click += executeButton_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.MenuText;
+            richTextBox1.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = SystemColors.Window;
+            richTextBox1.Location = new Point(771, 566);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(557, 122);
+            richTextBox1.TabIndex = 20;
+            richTextBox1.Text = "";
+            // 
+            // complexityLabel
+            // 
+            complexityLabel.AutoSize = true;
+            complexityLabel.Location = new Point(874, 148);
+            complexityLabel.Name = "complexityLabel";
+            complexityLabel.Size = new Size(38, 15);
+            complexityLabel.TabIndex = 21;
+            complexityLabel.Text = "label8";
+            // 
+            // asymptoticBehaviorLabel
+            // 
+            asymptoticBehaviorLabel.AutoSize = true;
+            asymptoticBehaviorLabel.Location = new Point(1026, 148);
+            asymptoticBehaviorLabel.Name = "asymptoticBehaviorLabel";
+            asymptoticBehaviorLabel.Size = new Size(38, 15);
+            asymptoticBehaviorLabel.TabIndex = 22;
+            asymptoticBehaviorLabel.Text = "label8";
+            // 
+            // mainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1340, 700);
+            Controls.Add(asymptoticBehaviorLabel);
+            Controls.Add(complexityLabel);
+            Controls.Add(richTextBox1);
+            Controls.Add(executeButton);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(performanceChart);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(capacityIncrementNumeric);
+            Controls.Add(itemsIncrementNumeric);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(resultsDataGridView);
+            Controls.Add(initialCapacityNumeric);
+            Controls.Add(executionCountNumeric);
+            Controls.Add(initialItemsNumeric);
+            Controls.Add(exePathTextBox);
+            Controls.Add(selectExeButton);
+            Name = "mainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)initialItemsNumeric).EndInit();
+            ((System.ComponentModel.ISupportInitialize)executionCountNumeric).EndInit();
+            ((System.ComponentModel.ISupportInitialize)initialCapacityNumeric).EndInit();
+            ((System.ComponentModel.ISupportInitialize)resultsDataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)capacityIncrementNumeric).EndInit();
+            ((System.ComponentModel.ISupportInitialize)itemsIncrementNumeric).EndInit();
+            ((System.ComponentModel.ISupportInitialize)performanceChart).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button selectExeButton;
+        private TextBox exePathTextBox;
+        private NumericUpDown initialItemsNumeric;
+        private NumericUpDown executionCountNumeric;
+        private NumericUpDown initialCapacityNumeric;
+        private DataGridView resultsDataGridView;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown capacityIncrementNumeric;
+        private NumericUpDown itemsIncrementNumeric;
+        private Label label4;
+        private Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart performanceChart;
+        private Label label6;
+        private Label label7;
+        private DataGridViewTextBoxColumn colInstance;
+        private DataGridViewTextBoxColumn colItems;
+        private DataGridViewTextBoxColumn colCapacity;
+        private DataGridViewTextBoxColumn colTime;
+        private DataGridViewTextBoxColumn colMemory;
+        private DataGridViewTextBoxColumn colExpectedValue;
+        private DataGridViewTextBoxColumn colSolutionValue;
+        private Button executeButton;
+        private RichTextBox richTextBox1;
+        private Label complexityLabel;
+        private Label asymptoticBehaviorLabel;
+    }
+}
---
+++ KPack01_Analyzer/Form1.cs
@@ -0,0 +1,245 @@
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using MathNet.Numerics;
+
+namespace KPack01_Analyzer
+{
+    public partial class mainForm : Form
+    {
+        public class ExecutionResult
+        {
+            public long ExecutionTime { get; set; }
+            public double PeakMemoryMB { get; set; }
+            public int SolutionValue { get; set; }
+        }
+
+        private Random randomGenerator = new Random();
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void selectExeButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Selecione o executável do algoritmo";
+            openFileDialog.Filter = "Executáveis (*.exe)|*.exe";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                exePathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            string exePath = exePathTextBox.Text;
+            if (!File.Exists(exePath))
+            {
+                MessageBox.Show("Executable not found at the specified path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            executeButton.Enabled = false;
+
+            resultsDataGridView.Rows.Clear();
+            performanceChart.Series["ExecutionTime"].Points.Clear();
+            performanceChart.Series["FittedCurve"].Points.Clear();
+
+            var xDataPoints = new List<double>();
+            var yDataPoints = new List<double>();
+
+            int executionCount = (int)executionCountNumeric.Value;
+            int currentItems = (int)initialItemsNumeric.Value;
+            int currentCapacity = (int)initialCapacityNumeric.Value;
+            int itemsIncrement = (int)itemsIncrementNumeric.Value;
+            int capacityIncrement = (int)capacityIncrementNumeric.Value;
+
+            for (int i = 0; i < executionCount; i++)
+            {
+                string tempInstanceFile = null;
+                try
+                {
+                    tempInstanceFile = GenerateArgumentString(currentCapacity, currentItems);
+
+                    ExecutionResult result = RunProcessAndGetResult(exePath, tempInstanceFile);
+
+                    resultsDataGridView.Rows.Add(
+                        i + 1,
+                        currentItems,
+                        currentCapacity,
+                        result.ExecutionTime,
+                        $"{result.PeakMemoryMB:F2}",
+                        "N/A",
+                        result.SolutionValue
+                    );
+
+                    xDataPoints.Add(currentItems);
+                    yDataPoints.Add(result.ExecutionTime);
+
+                    performanceChart.Series["ExecutionTime"].Points.AddXY(currentItems, result.ExecutionTime);
+
+                    Application.DoEvents();
+
+                    currentItems += itemsIncrement;
+                    currentCapacity += capacityIncrement;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An unexpected error occurred during execution: " + ex.Message, "Execution Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+                finally
+                {
+                    if (tempInstanceFile != null && File.Exists(tempInstanceFile))
+                    {
+                        File.Delete(tempInstanceFile);
+                    }
+                }
+            }
+
+            if (xDataPoints.Count > 2)
+            {
+                AnalyzeAndPlotBestFit(xDataPoints, yDataPoints);
+            }
+
+            executeButton.Enabled = true;
+            MessageBox.Show("Análise concluída!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private ExecutionResult RunProcessAndGetResult(string exePath, string arguments)
+        {
+            var result = new ExecutionResult();
+            var stopwatch = new Stopwatch();
+
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = exePath;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WorkingDirectory = Path.GetDirectoryName(exePath);
+
+                stopwatch.Start();
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                process.WaitForExit();
+                stopwatch.Stop();
+
+                result.ExecutionTime = stopwatch.ElapsedMilliseconds;
+
+                try
+                {
+                    result.PeakMemoryMB = process.PeakWorkingSet64 / 1024.0 / 1024.0;
+
+                    var outputLines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var line in outputLines)
+                    {
+                        if (line.StartsWith("Valor Solucao:", StringComparison.OrdinalIgnoreCase))
+                        {
+                            result.SolutionValue = int.Parse(line.Split(':')[1].Trim());
+                            break;
+                        }
+                    }
+                }
+                catch (InvalidOperationException) { }
+
+                return result;
+            }
+        }
+
+        private string GenerateArgumentString(int capacity, int itemCount)
+        {
+            var argumentBuilder = new StringBuilder();
+            argumentBuilder.Append(capacity).Append(" ").Append(itemCount).Append(" ");
+            var itemList = new List<string>();
+            for (int i = 0; i < itemCount; i++)
+            {
+                int value = randomGenerator.Next(10, 31);
+                int weight = randomGenerator.Next(2, 11);
+                itemList.Add($"{value},{weight}");
+            }
+            argumentBuilder.Append(string.Join(";", itemList));
+            return argumentBuilder.ToString();
+        }
+
+        private void AnalyzeAndPlotBestFit(List<double> xData, List<double> yData)
+        {
+            if (xData.Count < 3)
+            {
+                complexityLabel.Text = "Pontos insuficientes";
+                asymptoticBehaviorLabel.Text = "Execute mais instâncias";
+                return;
+            }
+
+            var xArray = xData.ToArray();
+            var yArray = yData.ToArray();
+            double meanY = yData.Average();
+            double ssTotal = yData.Sum(y => Math.Pow(y - meanY, 2));
+
+            var fitResults = new Dictionary<string, (Func<double, double> func, double r2Adjusted, int degree)>();
+
+            // Ajuste Linear: O(n)
+            var linear = Fit.Line(xArray, yArray);
+            Func<double, double> fLinear = x => linear.Item1 + linear.Item2 * x;
+            double r2Linear = CalculateAdjustedR2(fLinear, xData, yData, ssTotal, 1);
+            fitResults["O(n)"] = (fLinear, r2Linear, 1);
+
+            // Ajuste Quadrático: O(n²)
+            if (xData.Count >= 4)
+            {
+                var quad = Fit.Polynomial(xArray, yArray, 2);
+                Func<double, double> fQuad = x => quad[0] + quad[1] * x + quad[2] * x * x;
+                double r2Quad = CalculateAdjustedR2(fQuad, xData, yData, ssTotal, 2);
+                fitResults["O(n²)"] = (fQuad, r2Quad, 2);
+            }
+
+            // Ajuste Cúbico: O(n³)
+            if (xData.Count >= 5)
+            {
+                var cubic = Fit.Polynomial(xArray, yArray, 3);
+                Func<double, double> fCubic = x => cubic[0] + cubic[1] * x + cubic[2] * x * x + cubic[3] * x * x * x;
+                double r2Cubic = CalculateAdjustedR2(fCubic, xData, yData, ssTotal, 3);
+                fitResults["O(n³)"] = (fCubic, r2Cubic, 3);
+            }
+
+            var bestFit = fitResults.OrderByDescending(fr => fr.Value.r2Adjusted).First();
+            string bestLabel = bestFit.Key;
+            Func<double, double> bestFunc = bestFit.Value.func;
+
+            complexityLabel.Text = $"Complexidade estimada: {bestLabel}";
+            asymptoticBehaviorLabel.Text = $"R² ajustado: {bestFit.Value.r2Adjusted:F4}";
+
+            var fittedCurveSeries = performanceChart.Series["FittedCurve"];
+            fittedCurveSeries.Points.Clear();
+
+            for (double x = xData.Min(); x <= xData.Max(); x += (xData.Max() - xData.Min()) / 100.0)
+            {
+                fittedCurveSeries.Points.AddXY(x, bestFunc(x));
+            }
+        }
+
+        private double CalculateAdjustedR2(Func<double, double> func, List<double> x, List<double> y, double ssTotal, int degree)
+        {
+            double ssRes = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                ssRes += Math.Pow(y[i] - func(x[i]), 2);
+            }
+
+            int n = x.Count;
+            int p = degree + 1;
+
+            double r2 = 1 - ssRes / ssTotal;
+            double adjustedR2 = 1 - (1 - r2) * (n - 1) / (n - p - 1);
+            return adjustedR2;
+        }
+
+    }
+}
